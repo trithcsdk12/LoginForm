7ef5a6a0323cf7d91eb81d84af073a931e72c4a6
trithcsdk12 <trithcsdk12@gmail.com>
2025-02-17 15:49:33 +0700

    chỉnh sửa ảnh background

--- Login_Form/src/article/home.jsx
+++ Login_Form/src/article/home.jsx
@@ -3,7 +3,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 function Home() {
     return (
-        <img src="https://photo.znews.vn/w1920/Uploaded/mdf_eioxrd/2021_07_06/2.jpg" alt="" />
+        <img src="https://photo.znews.vn/w1920/Uploaded/mdf_eioxrd/2021_07_06/2.jpg" alt="background"  className="img-fluid"  style={{ width: '100%', height: 'auto' }} />
     )
 }
 
