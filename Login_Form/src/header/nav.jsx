698600528711aa49ccf2aa33141b6a41ed3028a2
trithcsdk12 <trithcsdk12@gmail.com>
2025-02-17 15:50:49 +0700

    chỉnh liên quan đến màu sắc, hiệu ứng và tương thích điện thoại(không chắc)

--- Login_Form/src/header/nav.jsx
+++ Login_Form/src/header/nav.jsx
@@ -41,8 +41,8 @@ function ChangeBackgroundColor({ color, setColor, setTxColor, txColor }) {
 function Nav({ user, setUser }) {
 
     const [users, setUsers] = useState([])
-    const [color, setColor] = useState('Blue');
-    const [txColor, setTxColor] = useState('White');
+    const [color, setColor] = useState('Gray');
+    const [txColor, setTxColor] = useState('Black');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -59,80 +59,91 @@ function Nav({ user, setUser }) {
 
     return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: color }}>
-                <div className="container-fluid">
-                    <Link to="/" className={`${styles.thumbnailLink}`}><img src="src/assets/logo_nav.webp" alt="" className='mx-5' style={{ width: '100px', height: '100px' }} /></Link>
+            <nav className="navbar navbar-expand-lg navbar-light py-3 mx-auto mb-5 sticky-top" id={`${styles.primaryNav}`} style={{ backgroundColor: color }}>
+                <div className="container-fluid d-flex justify-content-between">
+                    <Link to="/" className={`${styles.thumbnailLink}`}>
+                        <img src="src/assets/logo_nav.webp" alt="" className={`${styles.logo} mx-0`} style={{ width: '100px', height: '100px' }} />
+                    </Link>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
                         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/" style={{ color: txColor }}>Trang chủ</Link>
+                            <li className={` nav-item mx-3`}>
+                                <Link className={`${styles.iconHover} ${styles.iconContainer} nav-link`} aria-current="page" to="/" style={{ color: txColor }}>
+                                    <i className="fa-solid fa-house"></i>
+                                    <span className={`${styles.iconText}`}>Trang chủ</span>
+                                </Link>
                             </li>
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="colorDropdown" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false" style={{ color: txColor }}>
-                                    Đổi màu nền thanh Nav
+                            <li className="nav-item mx-3 dropdown">
+                                <a className={`${styles.iconHover} ${styles.iconContainer} nav-link dropdown-toggle`} href="#" id="colorDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: txColor }}>
+                                    <i className="fa-solid fa-droplet"></i>
+                                    <span className={`${styles.iconText}`}>Đổi màu nền</span>
                                 </a>
-                                <ul className="dropdown-menu" aria-labelledby="colorDropdown" >
+                                <ul className="dropdown-menu" aria-labelledby="colorDropdown">
                                     {backgroundColorList.map((colorObj, index) => (
-                                        <ChangeBackgroundColor key={index}
-                                            color={colorObj.bg_color}
-                                            txColor={colorObj.text_color}
-                                            setColor={setColor}
-                                            setTxColor={setTxColor} />
+                                        <ChangeBackgroundColor key={index} color={colorObj.bg_color} txColor={colorObj.text_color} setColor={setColor} setTxColor={setTxColor} />
                                     ))}
                                 </ul>
                             </li>
-                            {Boolean(user) ? <><span className="nav-link" style={{ color: txColor }}>
-                                Xin chào, {user.user.name}!
-                            </span>
-                                <button onClick={handleLogout}>
-                                    Đăng xuất
-                                </button>
-                            </>
-                                :
-                                <li className="nav-item dropdown">
-                                    <a className="nav-link dropdown-toggle" href="#" id="accountDropdown" role="button" data-bs-toggle="dropdown"
-                                        aria-expanded="false" style={{ color: txColor }}>
-                                        Tài khoản
+                            {Boolean(user) ? (
+                                <>
+                                    <span className="nav-link mx-3" style={{ color: txColor }}>
+                                        Xin chào, {user.user.name}!
+                                    </span>
+                                    <a onClick={handleLogout} className={`${styles.iconHover} ${styles.iconContainer}`}>
+                                        <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                                        <span className={`${styles.iconText}`}>Đăng xuất</span>
+                                    </a>
+                                </>
+                            ) : (
+                                <li className="nav-item mx-3">
+                                    <a className={`${styles.iconHover} ${styles.iconContainer} nav-link dropdown-toggle`} href="#" id="accountDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: txColor }}>
+                                        <i className="fa-solid fa-user"></i>
+                                        <span className={`${styles.iconText}`}>Tài khoản</span>
                                     </a>
                                     <ul className="dropdown-menu" aria-labelledby="accountDropdown">
-                                        <li><Link className="dropdown-item" to="/signIn">
-                                            Đăng nhập
-                                        </Link></li>
-                                        <li><Link className="dropdown-item" to="/signUp">Đăng ký</Link></li>
+                                        <li><Link className="dropdown-item" to="/signIn"><i className="fa-solid fa-arrow-right-to-bracket"></i> Đăng nhập</Link></li>
+                                        <li><Link className="dropdown-item" to="/signUp"><i className="fa-solid fa-arrow-turn-up"></i> Đăng ký</Link></li>
                                     </ul>
-                                </li>}
-                            <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-bs-toggle="dropdown"
-                                    aria-expanded="false" style={{ color: txColor }}>
-                                    Danh sách user
+                                </li>
+                            )}
+                            <li className="nav-item mx-3 dropdown">
+                                <a className={`${styles.iconHover} ${styles.iconContainer} nav-link dropdown-toggle`} href="#" id="userDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: txColor }}>
+                                    <i className="fa-solid fa-list"></i>
+                                    <span className={`${styles.iconText}`}>Danh sách user</span>
                                 </a>
-                                <ul className={` ${styles.menu} dropdown-menu`} aria-labelledby="userDropdown" >
+                                <ul className={` ${styles.menu} dropdown-menu`} aria-labelledby="userDropdown">
                                     {users.map(user => (
                                         <li key={user.id}>{user.email + ' Name: ' + user.firstName}<hr className="dropdown-divider" /></li>
                                     ))}
                                 </ul>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/product" style={{ color: txColor }}>Sản phẩm</Link>
+                            <li className="nav-item mx-3" id={`${styles.iconHover}`}>
+                                <Link className={`${styles.iconHover} ${styles.iconContainer} nav-link active`} aria-current="page" to="/product" style={{ color: txColor }}>
+                                    <i className="fa-solid fa-cart-plus"></i>
+                                    <span className={`${styles.iconText}`}>Sản phẩm</span>
+                                </Link>
                             </li>
-                            <li className="nav-item">
-                                <Link className="nav-link active" aria-current="page" to="/addToCart" style={{ color: txColor }}>Giỏ hàng</Link>
+                            <li className="nav-item mx-3">
+                                <Link className={`${styles.iconHover} ${styles.iconContainer} nav-link active`} aria-current="page" to="/addToCart" style={{ color: txColor }}>
+                                    <i className="fa-solid fa-cart-shopping"></i>
+                                    <span className={`${styles.iconText}`}>Giỏ hàng</span>
+                                </Link>
                             </li>
                         </ul>
                         <form className="d-flex">
                             <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                            <button className="btn btn-outline-success" type="submit" style={{ color: txColor }}>Tìm kiếm</button>
+                            <button className="btn btn-outline-success" type="submit" style={{ color: txColor }}>
+                                <i className="fa-solid fa-magnifying-glass"></i>
+                            </button>
                         </form>
                     </div>
                 </div>
             </nav>
         </>
-    )
+    );
 }
 
 export default Nav
